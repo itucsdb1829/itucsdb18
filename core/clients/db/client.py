bdd9d536ed6ab56a0e58aae65654d323b5f2b89c
SedatCagdas <sedatcagdas@gmail.com>
2018-11-12 11:39:08 +0300

    adds user create view

--- core/clients/db/client.py
+++ core/clients/db/client.py
@@ -7,7 +7,6 @@ class Client(object):
 
     def __init__(self):
         self.dsn = os.getenv('DATABASE_URL')
-        print(self.dsn)
         self.connection = db.connect(self.dsn)
         self.cursor = self.connection.cursor()
 
--- server.py
+++ server.py
@@ -1,19 +1,38 @@
-from flask import Flask
+import os
+
+from flask import Flask, request, Response
 
 from models.users import Users
 
 app = Flask(__name__)
 
 
-u = Users(name='sedat', surname='cagdas', email='sedat@test.com', role='admin', password='pw')
-u.create()
+# u = Users(name='sedat', surname='cagdas', email='sedat@test.com', role='admin', password='pw')
+# u.create()
 print('heloo')
 
+
+def test_method(name, surname, email, role, password):
+    print(name, surname, role, email, password)
 
 
 @app.route("/")
 def home_page():
     return 'sa'
+
+
+@app.route("/users/create", methods=['POST'])
+def create_user():
+    if request.method == 'POST':
+        if request.headers.get('Token') != os.getenv('AUTH_TOKEN'):
+            return Response(status=401)
+        user_data=dict()
+        for key, value in request.form.items():
+            user_data[key] = value
+
+        u = Users(**user_data)
+        u.create()
+    return Response(status=201)
 
 
 if __name__ == "__main__":
