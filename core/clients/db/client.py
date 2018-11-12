7a8ca004991d82884f9f7b69680620d67a0c4594
SedatCagdas <sedatcagdas@gmail.com>
2018-11-12 10:26:09 +0300

    fixes db client env for heroku

--- core/clients/db/client.py
+++ core/clients/db/client.py
@@ -6,7 +6,7 @@ import psycopg2 as db
 class Client(object):
 
     def __init__(self):
-        self.dsn = os.getenv('DSN')
+        self.dsn = os.getenv('DATABASE_URL')
         self.connection = db.connect(self.dsn)
         self.cursor = self.connection.cursor()
 
